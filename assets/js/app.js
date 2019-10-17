4f8bf53e28392ad6d89c9593585fd8a5ed705107
Maxime Pie <chamalowat@laposte.net>
2019-10-17 16:15:10 +0200

    feat(front): Add SASS and Javascript structure according to project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,30 @@
+// Javascript
+require('./heater.js');
+
+// SCSS
 require("../sass/app.scss");
+
+// =============================================================================
+// Require all scss files
+// =============================================================================
+require('../sass/0-basics/0-variables.scss');
+require('../sass/0-basics/1-init.scss');
+require('../sass/0-basics/2-fonts.scss');
+require('../sass/0-basics/3-small-elements.scss');
+require('../sass/1-navs/footer.scss');
+require('../sass/1-navs/menu.scss');
+require('../sass/1-navs/reassurances.scss');
+// require('../sass/2-modules/xxx.scss');
+// require('../sass/3-pages/xxx.scss');
+
+// =============================================================================
+// Require all js files
+// =============================================================================
+require('./partials/basics.js');
+
+// =============================================================================
+// Require dependencies
+// =============================================================================
+const $ = require('jquery');
+// require popper.js ?
+require('bootstrap');
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
 var Encore = require('@symfony/webpack-encore');
+
 Encore
 // the project directory where all compiled assets will be stored
   .setOutputPath('public/build/')
+
   // the public path used by the web server to access the previous directory
   .setPublicPath('/build')
+
   // will create public/build/app.js and public/build/app.css
   .addEntry('app', './assets/js/app.js')
-  // keeping all css in one single app.css file, compiled, minified, hard to read
-  // but good Encore practice would be to add new entry for each new css file that we want
+
   // allow legacy applications to use $/jQuery as a global variable
   .autoProvidejQuery()
+
   // enable source maps during development
   .enableSourceMaps(!Encore.isProduction())
+
   // empty the outputPath dir before each build
   .cleanupOutputBeforeBuild()
+
   // show OS notifications when builds finish/fail
   .enableBuildNotifications()
+
+  // create hashed filenames (e.g. app.abc123.css)
+   .enableVersioning()
+
   // allow sass/scss files to be processed
   .enableSassLoader()
+
+  // allow css treatment
+  // Automatically get his config in postcss.config.js file at root
   .enablePostCssLoader()
-// Automatically get his config in postcss.config.js file at root
 ;
+
 // export the final configuration
 module.exports = Encore.getWebpackConfig();
