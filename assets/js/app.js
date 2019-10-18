06c459cd47c63ca59a5fa2c4046d79798db01288
viviecam <camille.vivier@hotmail.fr>
2019-10-18 17:58:17 +0200

    Evol stack front

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,30 @@
 // SCSS
 require("../sass/app.scss");
 
+/// =============================================================================
+// REQURE ALL SCSS FILES
+// 
+// L'ordre est important, merci de ne rien modifier sans l'accord des responsables front
 // =============================================================================
-// Require all scss files
-// =============================================================================
-require('../sass/0-basics/0-variables.scss');
-require('../sass/0-basics/1-init.scss');
-require('../sass/0-basics/2-fonts.scss');
-require('../sass/0-basics/3-small-elements.scss');
-require('../sass/1-navs/footer.scss');
-require('../sass/1-navs/menu.scss');
-require('../sass/1-navs/reassurances.scss');
-require('../sass/3-pages/homepage.scss');
+
+// Bases
+require('../sass/bases/_init.scss');
+require('../sass/bases/_variables.scss');
+require('../sass/bases/_mixins.scss');
+require('../sass/bases/_fonts.scss');
+require('../sass/bases/_small-elements.scss');
+
+// Components
+require('../sass/components/forms.scss');
+
+// Navs
+require('../sass/layout-elements/footer.scss');
+require('../sass/layout-elements/menu.scss');
+require('../sass/layout-elements/sidebars.scss');
+require('../sass/layout-elements/reassurances.scss');
+
+// Pages
+require('../sass/pages/home.scss');
 
 // =============================================================================
 // Require dependencies
@@ -19,6 +32,7 @@ require('../sass/3-pages/homepage.scss');
 const $ = require('jquery');
 // require popper.js ?
 require('bootstrap');
+var unicons = require("unicons");
 
 // =============================================================================
 // Require all js files
