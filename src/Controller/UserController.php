7abdc9a7708b28e80c31cee6a0ce64ab0f2d2e3f
Maxime Pie <chamalowat@laposte.net>
2019-10-21 18:17:23 +0200

    doc(backoffice): Add comments on Backoffice Usercontroller ship method

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -56,13 +56,19 @@ class UserController extends AbstractController
     public function ship()
     {
         $user = $this->userRepository->findFirst();
+
+        // S'il n'y a pas de User en base de données on arrête tout !
         if (!$user) {
             return new Response('Pas d\'user');
         }
+
+        // On récupère le vaisseau du User
         $ship = $user->getShip();
 
         // Récupérer tous les canons avec la méthode $ship::getCanons();
 
+        // On affiche la vue ship.html.twig
+        // On lui envoie les paramètres 'controlleur_name', 'ship', 'user'. On peut envoyer tout ce qu'on veut !
         return $this->render('user/ship.html.twig', [
             'controller_name' => 'UserController',
             'ship' => $ship
