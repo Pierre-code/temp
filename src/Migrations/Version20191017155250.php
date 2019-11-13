021178ed9bc0746164b926def552b1c5f1134fb4
MaximePie <maxime.pie.mail@gmail.com>
2019-11-13 21:03:25 +0100

    config(backoffice): Fix some broken migration

--- src/Migrations/Version20191017155250.php
+++ src/Migrations/Version20191017155250.php
@@ -21,17 +21,17 @@ final class Version20191017155250 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('CREATE TABLE cafetiere (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, brand VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE user DROP email');
+        if ($schema->getTable('user')->hasColumn('email')) {
+            $this->addSql('ALTER TABLE user DROP email');
+        }
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP TABLE cafetiere');
-        $this->addSql('ALTER TABLE user ADD email VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
+        if (!$schema->getTable('user')->hasColumn('email')) {
+            $this->addSql('ALTER TABLE user ADD email VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
+        }
     }
 }
--- src/Migrations/Version20191027120328.php
+++ src/Migrations/Version20191027120328.php
@@ -22,7 +22,6 @@ final class Version20191027120328 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE planet (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('DROP TABLE user_search');
     }
 
@@ -32,6 +31,5 @@ final class Version20191027120328 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE user_search (id INT AUTO_INCREMENT NOT NULL, min_note INT NOT NULL, max_weight INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('DROP TABLE planet');
     }
 }
